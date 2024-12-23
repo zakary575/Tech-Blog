1958b430c8df6d12e3c06538a6292f926d457b47
Zakary Johnson <zakary.johnson575@gmail.com>
2024-12-23 14:15:20 -0600

    added a schema and the connection and started working on the controllers

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+// Import the routes. This is how we make our routes modular.
+
+
+// When a request is made to the /users or /projects path, it will be directed to the index.js in the /users or /projects folder.
+router.use();
+router.use();
+
+module.exports = router;
